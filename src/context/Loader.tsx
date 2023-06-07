4c2519eb5c294df460ee59dcad8ce1d62cf37501
Turmunkh <103709712+Turuu8@users.noreply.github.com>
2023-06-07 09:33:03 +0800

    Update Loader.tsx

--- src/context/Loader.tsx
+++ src/context/Loader.tsx
@@ -1,8 +1,8 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, ReactNode } from "react";
 
 const Loader = createContext<any | null>(null);
 
-export const LoaderProvider = ({ children }) => {
+export const LoaderProvider = ({ children }:{ children : ReactNode}) => {
   const [loading, setLoading] = useState(false);
 
   return <Loader.Provider value={{ loading, setLoading }}>{children}</Loader.Provider>;
